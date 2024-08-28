a1d7a1d9bfe68a84902f734f8b08c6fb82a9f0c8
Fertassi Amjad <125380494+Amjadfertassi@users.noreply.github.com>
2024-08-28 16:54:07 +0100

    --add colors and size

--- Ecommerce_App/lib/main.dart
+++ Ecommerce_App/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/utils/constants/colors.dart';
 import 'package:flutter_application_1/utils/theme/theme.dart';
 
 void main() {
